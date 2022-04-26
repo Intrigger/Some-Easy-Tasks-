0c1759e9d25767541a1aeab51033434ad0743d30
Daniel Kushko <xintrigger@gmail.com>
2022-04-27 01:38:54 +0300

    task done

---
+++ task_8.cpp
@@ -0,0 +1,29 @@
+// Тема 8: Файлы.
+
+// 8. Имеется текстовый файл, в каждой строке которого
+// первые два символа являются буквами.
+// Получить слово, образованное вторыми буквами каждой строки.
+
+
+#include <iostream>
+#include <fstream>
+
+
+int main(){
+
+    using namespace std;
+
+    ifstream fin;
+    fin.open("my_text_file.txt");
+
+    string str = "";
+
+    while (getline(fin, str)){
+
+        cout << str[1];
+
+    }
+
+
+    return 0;
+}
